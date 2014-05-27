2994ce096e965a61de345b483b1280c9fbb79bdd
Kevin McPhillips <github@kevinmcphillips.ca>
2014-05-27 17:31:45 -0400

    Simplify vagrant script

--- vagrant/vagrant.sh
+++ vagrant/vagrant.sh
@@ -8,18 +8,6 @@ echo "Configuring vagrant from kmcphillips/dotfiles"
 echo ""
 
 
-if which fish &> /dev/null; then
-  echo "**** Fish already installed at:"
-  which fish
-else
-  echo "**** Installing fish shell"
-  cd /tmp
-  sudo aptitude install bc
-  wget http://fishshell.com/files/2.1.0/linux/Ubuntu/fish_2.1.0-1~precise_amd64.deb
-  sudo dpkg -i fish_2.1.0-1~precise_amd64.deb
-fi
-
-
 echo ""
 echo "**** Cloning dotfiles"
 if [ ! -d "$SRC_DIR/dotfiles" ]; then
