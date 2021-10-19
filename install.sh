ef9fb6a508c18ff5f64c43a10b6875d0c1a78bda
Kevin McPhillips <github@kevinmcphillips.ca>
2021-10-18 22:25:52 -0400

    .gitattributes

--- install.sh
+++ install.sh
@@ -14,27 +14,33 @@ if [ $SPIN ]; then
     git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
   fi
 
+  cp -v .gitattributes ~/.gitattributes
+
 elif [[ $OSTYPE == 'darwin'* ]]; then
   echo 'Installing dotfiles for OSX..'
 
-  cp -v ./.zshrc ~/.zshrc
-  cp -v ./.zshrc_osx ~/.zshrc_osx
+  cp -v .zshrc ~/.zshrc
+  cp -v .zshrc_osx ~/.zshrc_osx
   echo 'source $HOME/.zshrc_osx' >> ~/.zshrc
 
   if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
     git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
   fi
 
+  cp -v .gitattributes ~/.gitattributes
+
 elif grep -q Ubuntu /etc/issue; then
   echo 'Installing dotfiles for Ubuntu...'
 
-  cp -v ./.zshrc ~/.zshrc
-  cp -v ./.zshrc_ubuntu ~/.zshrc_ubuntu
+  cp -v .zshrc ~/.zshrc
+  cp -v .zshrc_ubuntu ~/.zshrc_ubuntu
   echo 'source $HOME/.zshrc_ubuntu' >> ~/.zshrc
 
   if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
     git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
   fi
+
+  cp -v .gitattributes ~/.gitattributes
 
 else
   echo "Don't know how to install for this OS or env."
