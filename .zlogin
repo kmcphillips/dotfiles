46120b087b4253fd66147bc7579bcb8754dddd0e
Kevin McPhillips <github@kevinmcphillips.ca>
2025-02-19 12:34:49 -0500

    Remove Shopify

--- .zlogin
+++
@@ -1,8 +0,0 @@
-if [ "$TERM_PROGRAM" = "vscode" ]; then
-  cd "${OLDPWD}"
-else
-  SHOPIFY_REPO=$(cat /etc/spin/machine/constellation | perl -n -e'/shopify--(.*)\:/ && print $1')
-  if [ ! -z "${SHOPIFY_REPO}" ]; then
-    cd "${HOME}/src/github.com/Shopify/shopify/areas/core/shopify"
-  fi
-fi
--- install.sh
+++ install.sh
@@ -3,38 +3,13 @@
 
 cd "$( cd "$( dirname "$0" )" && pwd )"
 
-if [ $SPIN ]; then
-  echo 'Installing dotfiles for spin...'
-
-  sudo apt install exa -y
-
-  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-  cp -v ~/dotfiles/.zshrc ~/.zshrc
-  cp -v ~/dotfiles/.zshrc_spin ~/.zshrc_spin
-  echo 'source $HOME/.zshrc_spin' >> ~/.zshrc
-
-  if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
-    git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
-  fi
-
-  cp -v .vimrc ~/.vimrc
-  cp -v .gitattributes ~/.gitattributes
-  cp -v .gemrc ~/.gemrc
-  cp -v .zlogin ~/.zlogin
-  cat .gitconfig_spin >> ~/.gitconfig
-
-  git config advice.skippedCherryPicks false
-
-  ## This doesn't work because it needs to be done in context of Shopify/shopify ruby version
-  # gem install grb pry
-
-  git clone https://github.com/shopify/cursor-dotfiles ~/shopify-dotfiles/cursor-dotfiles
-  chmod +x ~/shopify-dotfiles/cursor-dotfiles/install.sh
-  ~/shopify-dotfiles/cursor-dotfiles/install.sh
-
-elif [[ $OSTYPE == 'darwin'* ]]; then
+if [[ $OSTYPE == 'darwin'* ]]; then
   echo 'Installing dotfiles for OSX..'
+
+  # Install oh-my-zsh
+  if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  fi
 
   cp -v .zshrc ~/.zshrc
   cp -v .zshrc_osx ~/.zshrc_osx
@@ -47,6 +22,7 @@ elif [[ $OSTYPE == 'darwin'* ]]; then
   cp -v .vimrc ~/.vimrc
   cp -v .gitattributes ~/.gitattributes
   cp -v .gemrc ~/.gemrc
+  cp -v .gitconfig >> ~/.gitconfig
 
 elif grep -q Ubuntu /etc/issue; then
   echo 'Installing dotfiles for Ubuntu...'
